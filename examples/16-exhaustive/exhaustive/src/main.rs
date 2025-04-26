3f6317b9da056bbc8c2b842c0ccb34c4aeae6b9e
Zain Ul Abdin <ghourimarti@gmail.com>
2025-04-26 08:58:26 +0500

    modified ..............

--- examples/16-exhaustive/exhaustive/src/main.rs
+++ examples/16-exhaustive/exhaustive/src/main.rs
@@ -7,11 +7,14 @@ enum WineGrapes {
 fn taste_wine(grapes: WineGrapes) {
     match grapes {
         WineGrapes::CabernetFranc => println!("This is a Cabertnet Franc wine."),
+        _ => println!("This is a NOT Cabertnet Franc wine."),
+
         // WineGrapes::Tannat => println!("This is a Tannat wine."),
         // WineGrapes::Merlot => println!("This is a Merlot wine."),
     }
 }
 
 fn main() {
+    println!("<=====================================>");
     taste_wine(WineGrapes::CabernetFranc);
 }
